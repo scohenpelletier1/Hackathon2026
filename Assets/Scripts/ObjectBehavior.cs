f5c9b9e7d1885d667843512abb749a928785cf3b
Sophia Cohen-Pelletier <scohenpelletier1@hwemail.com>
2026-02-16 15:09:56 -0800

    Finished Physics Objects (#3)
    * Added demo physics objects
    
    Box, spring, and junk :D
    
    * Box Physics
    
    Made the physics for the box, player can push it around
    
    * Finished Physics Objects
    
    All objects are movable. You cannot stand on top of the junk, you can stand on top of the box, and the spring shoots you upwards at 2 times the player's jump force

---
+++ Assets/Scripts/ObjectBehavior.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class ObjectBehavior : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool isTouchingPlayer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // get the rigidbody, set type
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // didn't delete bc we might put something in here later
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && this.CompareTag("Spring") && !collision.gameObject.GetComponent<PlayerController>().isGrounded)
+        {
+            print("Hiiiiiiiiiiiiiii");
+            GameObject player = collision.gameObject;
+
+            // if the collider is a player, the object IS a spring, and the player is standing on top of the spring
+            player.GetComponent<Rigidbody2D>().linearVelocityY = player.GetComponent<PlayerController>().jumpForce * 2;
+
+        } else if (collision.gameObject.CompareTag("Player"))
+        {
+            // if the collider is a player and the object is not a spring
+            print("player touched me");
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            isTouchingPlayer = true;
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // after player leaves, go back to kinematic
+            print("player stopped touching me");
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            rb.linearVelocityX = 0;
+            isTouchingPlayer = false;
+        }
+
+    }
+
+}
