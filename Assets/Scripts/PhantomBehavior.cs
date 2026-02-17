d7363ad7bf36635b48c07f1d3570273b00023953
Sophia <scohenpelletier1@hwemail.com>
2026-02-16 20:16:44 -0800

    Phantom Objects
    Created phantom objects that when clicked, create the real objects :0 mind blown

---
+++ Assets/Scripts/PhantomBehavior.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class PhantomBehavior : MonoBehaviour
+{
+    public GameObject nonPhantomObject;
+    public String nonPhantomObjectName;
+    public Camera mainCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // find mouse position relative to camera
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+
+        // set the phantom's position
+        transform.position = worldPosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // create the non phantom object
+            Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+            nonPhantomObject = GameObject.Instantiate(nonPhantomObject);
+            nonPhantomObject.transform.position = spawnPosition;
+            nonPhantomObject.name = nonPhantomObjectName;
+
+            // delete self
+            Destroy(this);
+        }
+
+    }
+
+}
