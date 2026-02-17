6f6d1426f933cb4c2f899d42a71267217e823b7b
Sophia Cohen-Pelletier <scohenpelletier1@hwemail.com>
2026-02-17 04:00:29 -0800

    Created Filaments (#12)
    Created a filament sprite that you can pick up and add to your total filament count

---
+++ Assets/Scripts/FilamentBehavior.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FilamentBehavior : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            // add to number of filaments
+            GameManager.Gary.addFilament(1);
+
+            // destroy the filament
+            Destroy(this.gameObject);
+
+        }
+        
+    }
+}
